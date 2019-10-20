f853513e57d8119d60611da66534b12a3d6745f9
David Matas <adavidmatas@gmail.com>
2019-10-20 11:26:31 +0200

    Preset for Elixir

---
+++ presets/elixir/lib/game_of_life.ex
@@ -0,0 +1,18 @@
+defmodule GameOfLife do
+  @moduledoc """
+  Documentation for GameOfLife.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GameOfLife.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
